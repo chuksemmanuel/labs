2f0a0c78c71e79972645cbb4cbce3930e0937178
Chuks Emmanuel <dev.chuksemmanuel@gmail.com>
2025-05-23 07:30:01 +0100

    feat: setup basic form using plain react-hook-form

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 'use client';
-
 import { SubmitHandler, useForm } from 'react-hook-form';
 
 type FormFields = {
@@ -11,10 +10,40 @@ const ReactHookForm = () => {
 	const {
 		register,
 		handleSubmit,
-		formState: { errors },
-	} = useForm<FormFields>();
-	const onSubmit: SubmitHandler<FormFields> = data => {
-		console.log(data);
+		setError,
+		formState: { errors, isSubmitting },
+	} = useForm<FormFields>({
+		defaultValues: {
+			email: 'test@email.com',
+		},
+	});
+
+	const onSubmit: SubmitHandler<FormFields> = async data => {
+		try {
+			await new Promise(resolve => setTimeout(resolve, 1000));
+			console.log(data);
+			// throw new Error(JSON.stringify({ type: 'email', message: 'Email already taken' }));
+			throw new Error('Something went wrong!');
+		} catch (error) {
+			let type, message;
+			if (error instanceof Error) {
+				try {
+					const parsed = JSON.parse(error.message);
+					type = parsed.type;
+					message = parsed.message;
+				} catch {
+					// fallback if not JSON
+					type = undefined;
+					message = error.message;
+				}
+			}
+
+			if (type === 'email') {
+				setError('email', { message });
+			} else {
+				setError('root', { message });
+			}
+		}
 	};
 	return (
 		<div className='page-width'>
@@ -22,9 +51,17 @@ const ReactHookForm = () => {
 				<div className='flex flex-col gap-0.5'>
 					<input
 						{...register('email', {
-							required: true,
-							pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/,
-							validate: value => value !== 'admin',
+							required: 'Email is required',
+							pattern: {
+								value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+								message: 'Invalid email address',
+							},
+							validate: value => {
+								if (value.toLowerCase().includes('admin')) {
+									return 'Email most not include admin';
+								}
+								return true;
+							},
 						})}
 						type='text'
 						placeholder='email'
@@ -36,19 +73,27 @@ const ReactHookForm = () => {
 				<div className='flex flex-col gap-0.5'>
 					<input
 						{...register('password', {
-							required: true,
-							minLength: 8,
+							required: 'Password is required:',
+							minLength: {
+								value: 8,
+								message: 'Password must have at least 8 characters:',
+							},
 						})}
 						type='password'
 						placeholder='Password'
 						className='border border-gray-400 rounded px-4 py-2'
 					/>
+					{errors.password && <span className='text-red-500 text-xs'>{errors.password.message}</span>}
 				</div>
-				<button
-					type='submit'
-					className='bg-purple-500 text-white rounded py-2 px-4 font-medium cursor-pointer hover:bg-purple-600 active:bg-purple-700 '>
-					Submit
-				</button>
+				<div className='flex flex-col gap-0.5'>
+					<button
+						type='submit'
+						className='bg-purple-500 text-white rounded py-2 px-4 font-medium cursor-pointer hover:bg-purple-600 active:bg-purple-700 '
+						disabled={isSubmitting}>
+						{isSubmitting ? 'Submitting...' : 'Submit'}
+					</button>
+					{errors.root && <span className='text-red-500 text-xs'>{errors.root.message}</span>}
+				</div>
 			</form>
 		</div>
 	);
