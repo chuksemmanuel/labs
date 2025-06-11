ad93148b0155d181b97dd92d8589bd6cc6da540c
Chuks Emmanuel <dev.chuksemmanuel@gmail.com>
2025-06-11 02:55:48 +0100

    add grids

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,16 @@ import React from 'react';
 
 const page = () => {
 	return (
-		<div>
-			<div className='page-width'>
-				<h1 className='text-4xl font-bold'>Next React Lab</h1>
-			</div>
-		</div>
+		<>
+			<main className='boxes'>
+				<div className='box w-full  bg-gray-700 p-20 text-8xl flex justify-center items-center font-light text-white box-span-2'>1</div>
+				<div className='box w-full  bg-orange-700 p-20 text-8xl flex justify-center items-center font-light text-white'>2</div>
+				<div className='box w-full  bg-purple-700 p-20 text-8xl flex justify-center items-center font-light text-white'>3</div>
+				<div className='box w-full  bg-blue-700 p-20 text-8xl flex justify-center items-center font-light text-white box-span-2'>4</div>
+				<div className='box w-full  bg-pink-700 p-20 text-8xl flex justify-center items-center font-light text-white'>5</div>
+				<div className='box w-full  bg-green-700 p-20 text-8xl flex justify-center items-center font-light text-white'>6</div>
+			</main>
+		</>
 	);
 };
 
